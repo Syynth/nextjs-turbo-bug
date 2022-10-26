0f431015d5ddacf61de991bb9ae4e4093c416264
Syynth <ben.cochrane2112@gmail.com>
2022-10-26 11:50:05 -0400

    1 file modified, 1 added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import FormData from 'form-data';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // just make sure it's used by bundler
+  console.log(FormData);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
